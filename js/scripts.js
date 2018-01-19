569b69cb16024fcfd9933b6a141f137b5e807953
Sara Hamilton <Guest@Epicodus-9C.local>
2018-01-19 10:56:54 -0800

    add prompts if data is not entered

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var beepBoop = function(rangeValue) {
   var numbers = [];
   for (var index = 1; index <= rangeValue; index += 1) {
     if (index.toString().includes("3")) {
-      numbers.push("I'm sorry, Dave. I'm afraid I can't do that.")
+      numbers.push("sorry")
     } else if (index.toString().includes("1")) {
       numbers.push("Boop!")
     } else if (index % 10 === 0) {
@@ -23,9 +23,15 @@ $(document).ready(function() {
     var rangeValue = parseInt($("input#rangeValue").val());
     var results = beepBoop(rangeValue);
 
+    if (!name) {
+      name = prompt("Please enter a name.");
+    } else if (!rangeValue) {
+      rangeValue = prompt("Please enter a number.");
+    }
+
     results.forEach(function(result) {
       console.log(result);
-      if ((name) && (result === ("I'm sorry, Dave. I'm afraid I can't do that."))) {
+      if (result === ("sorry")) {
         result = ("I'm sorry, " + name + ". I'm afraid I can't do that.")
       } else {
         result = result;
