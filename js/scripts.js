c5da36340e637733e8c52d056e61f16a2402c3b9
Sara Hamilton <Guest@Epicodus-9C.local>
2018-01-19 09:08:58 -0800

    add basic js function

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
-$(document).ready(function(){
-  $("#beepBoopForm").submit(function(event){
+var beepBoop = function(rangeValue) {
+  return false;
+}
+
+$(document).ready(function() {
+  $("#beepBoopForm").submit(function(event) {
     event.preventDefault();
 
-    var rangeValue = parseInt("input#rangeValue").val());
+    var rangeValue = parseInt($("input#rangeValue").val());
+    var result = beepBoop(rangeValue);
 
     $("#result").text(result);
-  })
+  });
 });
